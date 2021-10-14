dd1eeac9965992f64ee4062c138db4ecfc94c488
Monica Silva Lopez <msilva@correo.ler.uam.mx>
2021-10-13 20:45:25 -0500

    interpolacion de nombres

---
+++ interpolacion01.js
@@ -0,0 +1,9 @@
+const nombre = "Monica"
+const apellido = "Silva"
+const pais = "Mexico"
+
+const cadena = nombre + "-" + apellido + "-" + pais
+const cadena2 = `${ nombre } - ${ apellido } - ${ pais }`
+
+console.log(cadena)
+console.log(cadena2) 
