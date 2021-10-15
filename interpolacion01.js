4ea06edd16aa847301f81452066d307ea5c8f897
Monica Silva Lopez <msilva@correo.ler.uam.mx>
2021-10-15 12:03:50 -0500

    error en el contenido de interpolacion

--- interpolacion01.js
+++ interpolacion01.js
@@ -4,8 +4,11 @@ const pais = "Mexico"
 
 const cadena = nombre + "-" + apellido + "-" + pais
 const cadena2 = `${ nombre } - ${ apellido } - ${ pais }`
+//ejemplo de otras opciones para concatenar.
 const cadena3 = `${ 1+1 } - ${ Math.abs(-1) } `
-
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+xxxxxxxxxxxxxxxx
 console.log(cadena)
 console.log(cadena2)
 console.log(cadena3)
